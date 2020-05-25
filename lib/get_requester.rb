e0321ede95ae6b5b8dcee18392347b216643bce1
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-25 20:06:41 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -22,4 +22,18 @@ require 'json'
 # end
 
 
+class GetRequester
+  def initialize(url)
+    @url = url
+  end
 
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(get_response_body)
+  end
+end
