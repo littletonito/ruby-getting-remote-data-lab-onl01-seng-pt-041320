a7623d65f135074b517b7fa6e00dc750b7ef98ba
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-25 20:07:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,37 +3,36 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 
-# class GetRequester
-  
-#   def initialize(url)
-#     @url = url
-#   end
-  
-#   def get_response_body
-#     url = URI.parse(@url)
-#     response = Net::HTTP.get_response(uri)
-#     response.body
-#   end
-  
-#   def parse_json
-#     JSON.parse(get_response_body)
-#   end
-  
-# end
-
-
 class GetRequester
+  
   def initialize(url)
     @url = url
   end
-
+  
   def get_response_body
-    uri = URI.parse(@url)
+    url = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
-
+  
   def parse_json
     JSON.parse(get_response_body)
   end
+  
 end
+
+# class GetRequester
+#   def initialize(url)
+#     @url = url
+#   end
+
+#   def get_response_body
+#     uri = URI.parse(@url)
+#     response = Net::HTTP.get_response(uri)
+#     response.body
+#   end
+
+#   def parse_json
+#     JSON.parse(get_response_body)
+#   end
+# end
