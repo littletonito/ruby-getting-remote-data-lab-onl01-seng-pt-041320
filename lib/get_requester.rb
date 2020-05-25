de96089b5643e9bc6ae9175e280f6beb7d1856db
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-25 20:06:07 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -11,7 +11,7 @@ class GetRequester
   
   def get_response_body
     url = URI.parse(@url)
-    response = Net::HTTP.get_response_body(uri)
+    response = Net::HTTP.get_response(uri)
     response.body
   end
   
